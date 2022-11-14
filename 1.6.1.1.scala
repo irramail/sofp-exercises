2882dbc31a544ff894174ab6175260926627edb5
irramail <irramail@gmail.com>
2022-11-15 02:24:26 +0700

    Update 1.6.1.1.scala

--- 1.6.1.1.scala
+++ 1.6.1.1.scala
@@ -1,4 +1,7 @@
+// f!!
+
 ((n: Int) => if (n % 2 == 0) (2 to n by 2).product else (1 to n by 2).product)(8)
-384
+// 384
+
 ((n: Int) => if (n % 2 == 0) (2 to n by 2).product else (1 to n by 2).product)(9)
-945
+// 945
