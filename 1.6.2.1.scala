faf61ae6c7b23a42f7c3dcc68c496530c6428c85
irramail <irramail@gmail.com>
2022-11-20 14:38:23 +0700

    Create 1.6.2.1.scala

---
+++ 1.6.2.1.scala
@@ -0,0 +1,5 @@
+def add20(s: List[List[Int]]): List[List[Int]] = s.map(l => l.map(n => n+20))
+//def add20(s: List[List[Int]]): List[List[Int]]
+
+add20( List( List(1), List(2, 3) ) )
+//val res3: List[List[Int]] = List(List(21), List(22, 23))
